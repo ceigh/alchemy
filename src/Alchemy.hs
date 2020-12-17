345a356d2eb08feac36d3c95c089f38a1bd5f85f
Artjom Löbsack <me@ceigh.com>
2020-12-17 18:39:45 +0500

    feat: add basic types

--- src/Alchemy.hs
+++ src/Alchemy.hs
@@ -1,1 +1,29 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Alchemy where
+
+import Lens.Micro.TH (makeLenses)
+
+-- types
+
+data Element = Element
+  { _name :: String
+  , _desc :: String
+  } deriving Show
+
+data HistoryRecord = HistoryRecord
+  { _from :: Element
+  , _to   :: Element
+  } deriving Show
+
+data Game = Game
+  { _openedElements        :: [Element]
+  , _allElements           :: [Element]
+  , _fstColElements        :: [Element]
+  , _sndColElements        :: [Element]
+  , _fstColSelectedElement :: Element
+  , _history               :: [HistoryRecord]
+  } deriving Show
+
+makeLenses ''Element
+makeLenses ''HistoryRecord
+makeLenses ''Game
